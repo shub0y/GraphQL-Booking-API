4d1016a863ac86c848e203a533e7b7b4cc912eb9
shub0y <gadewarshubham002@gmail.com>
2019-01-15 11:13:37 -0500

    Added query and mutations

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {}
+}));
+
+app.listen(3000);
