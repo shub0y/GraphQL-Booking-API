9ec966c89e252643c2a5522b4b1d94761bd32965
shub0y <gadewarshubham002@gmail.com>
2019-01-22 16:53:06 -0500

    adding created events

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ app.use(
           description: String!
           price: Float!
           date: String!
+          creator: User!
         }
         type User {
           _id: ID!
           email: String!
           password: String
+          createdEvents: [Event!]
         }
         input EventInput {
           title: String!
@@ -53,9 +55,17 @@ app.use(
     rootValue: {
       events: () => {
         return Event.find()
+        .populate('creator')
           .then(events => {
             return events.map(event => {
-              return { ...event._doc, _id: event.id };
+              return { 
+                ...event._doc, 
+                _id: event.id,
+                creator: {
+                    ...event._doc.creator._doc,
+                    _id: event._doc.creator.id
+                }
+                };
             });
           })
           .catch(err => {
